c100c60edfe29b502cba14b04e9f0e93084e5bfb
vinduja <31876072+vinduja@users.noreply.github.com>
2017-09-12 12:02:00 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		int temp,i;
+		int a[10]={1,2,3,4,5,6,7,9,8,10};
+		{s
+		for(i=0;i<0,i++)
+		{
+		temp=a[i];
+		System.Out.Println("read same array length");
+		}
+		}	
+
+	}
+
+}
